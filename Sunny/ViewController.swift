88dde1157f7549adea90161ae2bdcca9325e035f
石井翔真 <>
2019-12-06 11:06:45 +0900

    frist commit

---
+++ Sunny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+//  Created by 石井翔真 on 2019/12/06.
+//  Copyright © 2019 石井翔真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
