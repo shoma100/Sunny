53d55b50a7db9003002d64c2d7fcf107a9ebd4fe
原涼馬 <>
2019-12-17 14:38:33 +0900

    ログイン等実装

--- Sunny/topViewController.swift
+++ Sunny/topViewController.swift
@@ -8,10 +8,46 @@
 
 import Foundation
 import UIKit
+import FirebaseUI
 
-class topViewController: UIViewController {
+class topViewController: UIViewController,FUIAuthDelegate {
+    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet weak var signIn: UIButton!
+    @IBOutlet weak var google: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // authUIのデリゲート
+        self.authUI.delegate = self
+        self.authUI.providers = providers
+        AuthButton.addTarget(self,action: #selector(self.AuthButtonTapped(sender:)),for: .touchUpInside)
+        signIn.layer.cornerRadius = 5.0
+        signUp.layer.cornerRadius = 5.0
+        google.layer.cornerRadius = 5.0
+        
+    }
+    
+    @IBOutlet weak var AuthButton: UIButton!
+
+    var authUI: FUIAuth { get { return FUIAuth.defaultAuthUI()!}}
+    // 認証に使用するプロバイダの選択
+    let providers: [FUIAuthProvider] = [
+        FUIGoogleAuth()
+        ]
+    
+    @objc func AuthButtonTapped(sender : AnyObject) {
+        // FirebaseUIのViewの取得
+        let authViewController = self.authUI.authViewController()
+        // FirebaseUIのViewの表示
+        self.present(authViewController, animated: true, completion: nil)
+    }
+
+    //　認証画面から離れたときに呼ばれる（キャンセルボタン押下含む）
+    public func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?){
+        // 認証に成功した場合
+        if error == nil {
+            self.performSegue(withIdentifier: "toTopView", sender: self)
+        }
+        // エラー時の処理をここに書く
     }
 }
