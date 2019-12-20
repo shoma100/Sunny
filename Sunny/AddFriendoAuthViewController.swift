c50a904ae1aafea7ccefb0250fc5eeea89105fdf
石井翔真 <>
2019-12-20 13:55:55 +0900

    QRコードに、Sunnyコードのみを取得させる機能追加

---
+++ Sunny/AddFriendoAuthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddFriendoAuthViewController.swift
+//  Sunny
+//
+//  Created by 石井翔真 on 2019/12/19.
+//  Copyright © 2019 石井翔真. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class AddFriendAuthViewController: UIViewController {
+    var id:String?
+    @IBOutlet weak var name: UILabel!
+    let ref = Database.database().reference()
+
+    
+    
+    override func viewDidLoad() {
+  
+        print(id)
+        let user = Auth.auth().currentUser
+        //        uid.text = user?.uid
+                
+                ref.child("user").child(id!).observe(.value) { (snapshot) in
+                    let data = snapshot.value as? [String : AnyObject] ?? [:]
+                    let name = data["name"] as? String
+//                    let id = data["id"] as? String
+                    self.name.text = name!
+                   // self.uid.text = "id：" + id!
+                 }
+    
+    }
+}
---
+++ Sunny/StringExt.swift
@@ -0,0 +1,50 @@
+//
+//  StringExt.swift
+//  Sunny
+//
+//  Created by 石井翔真 on 2019/12/20.
+//  Copyright © 2019 石井翔真. All rights reserved.
+//
+
+//正規表現のクラス
+
+import Foundation
+
+extension String {
+    //絵文字など(2文字分)も含めた文字数を返します
+    var length: Int {
+        let string_NS = self as NSString
+        return string_NS.length
+    }
+
+    //正規表現の検索をします
+    func pregMatche(pattern: String, options: NSRegularExpression.Options = []) -> Bool {
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: options) else {
+            return false
+        }
+        let matches = regex.matches(in: self, options: [], range: NSMakeRange(0, self.length))
+        return matches.count > 0
+    }
+
+    //正規表現の検索結果を利用できます
+    func pregMatche(pattern: String, options: NSRegularExpression.Options = [], matches: inout [String]) -> Bool {
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: options) else {
+            return false
+        }
+        let targetStringRange = NSRange(location: 0, length: self.length)
+        let results = regex.matches(in: self, options: [], range: targetStringRange)
+        for i in 0 ..< results.count {
+            for j in 0 ..< results[i].numberOfRanges {
+                let range = results[i].range(at: j)
+                matches.append((self as NSString).substring(with: range))
+            }
+        }
+        return results.count > 0
+    }
+
+    //正規表現の置換をします
+    func pregReplace(pattern: String, with: String, options: NSRegularExpression.Options = []) -> String {
+        let regex = try! NSRegularExpression(pattern: pattern, options: options)
+        return regex.stringByReplacingMatches(in: self, options: [], range: NSMakeRange(0, self.length), withTemplate: with)
+    }
+}
