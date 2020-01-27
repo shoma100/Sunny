5277cda519866d8ed029b756998caeccf1d1ad97
原涼馬 <>
2020-01-27 09:36:49 +0900

    フレンドのid検索実装

--- Sunny/AddFriendViewController.swift
+++ Sunny/AddFriendViewController.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class AddFriendViewController: UIViewController {
     
+    let ref = Database.database().reference()
     @IBOutlet weak var friendId: UITextField!
+    var uid = ""
+    
     override func viewDidLoad() {
         
     }
@@ -20,41 +24,45 @@ class AddFriendViewController: UIViewController {
         super.didReceiveMemoryWarning()
         
     }
+    @IBAction func search(_ sender: Any) {
+        idSearch(id: friendId.text!, complete: {
+            uid in
+            print(uid)
+            self.uid = uid
+            self.performSegue(withIdentifier: "toAddFriendCheck", sender: nil)
+        })
+    }
     
-//    func Search() {
-//        friendSearch(id: "id", complete: {
-//            friend in
-//
-//        })
-//    }
-//
-//    func friendSearch(id: String, complete: @escaping (Dictionary<String, Any>) -> Void) {
-//        ref.child("user").child(id).observe(.value) { (snapshot) in
-//            // Dictionary型にキャスト
-//            let user = snapshot.value as! [String: Any]
-//            let friendString = user["name"] as! String
-//            complete(user)
-//        }
-//    }
-    
+    func idSearch(id: String, complete: @escaping (String) -> Void) {
+        ref.child("user").observe(.value) { (snapshot) in
+            //            Users直下のデータの数だけ繰り返す。
+            for data in snapshot.children {
+                let snapData = data as! DataSnapshot
+                // Dictionary型にキャスト
+                let user = snapData.value as! [String: Any]
+                if id == user["id"] as! String {
+                    complete(snapData.key)
+                }
+            }
+        }
+    }
     /// セグエ実行前処理
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let identifier = segue.identifier else {
-                // identifierが取れなかったら処理やめる
+            // identifierが取れなかったら処理やめる
             return
         }
-        if(identifier == "toAddFriend") {
-                // NavigationControllerへの遷移の場合
-
-                // segueから遷移先のNavigationControllerを取得
+        if(identifier == "toAddFriendCheck") {
+            // NavigationControllerへの遷移の場合
+            
+            // segueから遷移先のNavigationControllerを取得
             let vc = segue.destination as! AddFriendAuthViewController
-                // 次画面のテキスト表示用のStringに、本画面のテキストフィールドのテキストを入れる
-            vc.id = self.friendId.text!
+            // 次画面のテキスト表示用のStringに、本画面のテキストフィールドのテキストを入れる
+            vc.id = uid
         }
-//        let next = segue.destination as? AddFriendAuthViewController
-//        let _ = next?.view
-//        next?.textField3.text = textField.text
+        //        let next = segue.destination as? AddFriendAuthViewController
+        //        let _ = next?.view
+        //        next?.textField3.text = textField.text
     }
-    
-    
+
 }
