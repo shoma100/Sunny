e497a966aeb5574cea1ac9a491ee38de3d3e3009
権藤　俊 <gondo.syun0711@gmail.com>
2020-01-29 12:18:37 +0900

    プロフィール画像の変更、DB登録、表示を実装

---
+++ Sunny/UIView+Border.swift
@@ -0,0 +1,44 @@
+//
+//  UIView+Border.swift
+//  Where
+//
+//  Created by しゅん on 2019/12/19.
+//  Copyright © 2019 g-chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
