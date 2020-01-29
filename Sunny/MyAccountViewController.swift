159a53baa16db8086474128ed64d597c4afcd6fa
権藤　俊 <gondo.syun0711@gmail.com>
2020-01-29 12:37:10 +0900

    アカウント画面のプロフィール画像にも適用されるように実装

--- Sunny/MyAccountViewController.swift
+++ Sunny/MyAccountViewController.swift
@@ -13,19 +13,38 @@ class MyAccountViewController: UIViewController {
     let ref = Database.database().reference()
     @IBOutlet weak var uid: UILabel!
     @IBOutlet weak var unm: UILabel!
+    @IBOutlet weak var accountImg: UIImageView!
     
     override func viewDidLoad() {
         
-        let user = Auth.auth().currentUser
-//        uid.text = user?.uid
+        // 角を丸くする
+        accountImg.contentMode = .scaleAspectFill
+        accountImg.layer.cornerRadius = accountImg.frame.width/2
+        accountImg.clipsToBounds = true
         
-        ref.child("user").child(user!.uid).observe(.value) { (snapshot) in
-            let data = snapshot.value as? [String : AnyObject] ?? [:]
-            var name = data["name"] as? String
-            var id = data["id"] as? String
-            self.unm.text = "ユーザ名：" + name!
-            self.uid.text = "id：" + id!
-         }
+        if let user = Auth.auth().currentUser {
+            if let accountImgURL = user.photoURL {
+                do {
+                    let url = URL(string: accountImgURL.absoluteString)
+                    let data = try Data(contentsOf: url!)
+                    self.accountImg.image = UIImage(data: data)
+                } catch {
+                    print(error)
+                }
+            } else {
+                self.accountImg.image = UIImage(named: "AppIcon")
+            }
+            
+            ref.child("user").child(user.uid).observe(.value) { (snapshot) in
+                let data = snapshot.value as? [String : AnyObject] ?? [:]
+                var name = data["name"] as? String
+                var id = data["id"] as? String
+                self.unm.text = "ユーザ名：" + name!
+                self.uid.text = "id：" + id!
+            }
+        }
+        
+        
     }
     
     @IBAction func myUnwindAction(segue: UIStoryboard) {
